9817113fcdce535646c64c3b08f989c454a4b87b
jarvo91 <37822934+jarvo91@users.noreply.github.com>
2018-04-02 17:04:12 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Learn.Python3.the.Hard.Way
+
+my answers to Learn Python 3 the hard way by zed A. Shaw
---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("how old are you", end=' ')
+# end=' ' tells print line to not end line with new line character
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height}cm tall and {weight}kg heavy.")
